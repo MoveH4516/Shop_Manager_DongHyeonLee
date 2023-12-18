f410f34803e32c1c9bbfaf08b1489add64a19e9c
MoveH4516 <ldh5045@naver.com>
2023-12-18 11:17:48 +0900

    .

--- Shop/src/_Main/_Main.java
+++ Shop/src/_Main/_Main.java
@@ -1,10 +1,14 @@
 package _Main;
 
+import Controller.ShopController;
+
 public class _Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ShopController shop = new ShopController();
+		shop.run();
+		
 	}
 
 }
